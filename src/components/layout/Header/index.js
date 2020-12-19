aee37258a8e774de340fa74a6483885ea046b094
Krishal Shah <krishal.shah@Krishals-MacBook-Pro.local>
2020-12-20 00:07:44 +0530

    UPDATE/Added linting

--- src/components/layout/Header/index.js
+++ src/components/layout/Header/index.js
@@ -1,33 +1,33 @@
 import React from "react";
 
-import { Avatar } from "@material-ui/core";
-import { AccessTimeOutlined, SearchOutlined, HelpOutlineOutlined } from "@material-ui/icons";
+import {Avatar} from "@material-ui/core";
+import {AccessTimeOutlined, SearchOutlined, HelpOutlineOutlined} from "@material-ui/icons";
 
 import "./style.css";
-import { useStateValue } from "../../../state/StateProvider";
+import {useStateValue} from "../../../state/StateProvider";
 
 function Header() {
-    const [{user}] = useStateValue();
+	const [{user}] = useStateValue();
 
-    return (
-        <div className="header">
-            <div className="header__left">
-                <Avatar
-                    className="header__avatar"
-                    alt={user?.displayName}
-                    src={user?.photoURL}
-                />
-                <AccessTimeOutlined />
-            </div>
-            <div className="header__search">
-                <SearchOutlined />
-                <input placeholder="Search" />
-            </div>
-            <div className="header__right">
-                <HelpOutlineOutlined />
-            </div>
-        </div>
-    );
+	return (
+		<div className="header">
+			<div className="header__left">
+				<Avatar
+					className="header__avatar"
+					alt={user?.displayName}
+					src={user?.photoURL}
+				/>
+				<AccessTimeOutlined />
+			</div>
+			<div className="header__search">
+				<SearchOutlined />
+				<input placeholder="Search" />
+			</div>
+			<div className="header__right">
+				<HelpOutlineOutlined />
+			</div>
+		</div>
+	);
 };
 
 export default Header;
--- src/components/layout/Sidebar/index.js
+++ src/components/layout/Sidebar/index.js
@@ -1,68 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
 import {
-    FiberManualRecordRounded,
-    CreateRounded,
-    InsertCommentRounded,
-    InboxRounded,
-    DraftsRounded,
-    BookmarkBorderRounded,
-    PeopleAltRounded,
-    AppsRounded,
-    FileCopyRounded,
-    ExpandLessRounded,
-    ExpandMoreRounded,
-    AddRounded,
+	FiberManualRecordRounded,
+	CreateRounded,
+	InsertCommentRounded,
+	InboxRounded,
+	DraftsRounded,
+	BookmarkBorderRounded,
+	PeopleAltRounded,
+	AppsRounded,
+	FileCopyRounded,
+	ExpandLessRounded,
+	ExpandMoreRounded,
+	AddRounded,
 } from "@material-ui/icons";
 
 import SidebarOption from "./SidebarOption";
 import db from "../../../services/firebase";
-import { useStateValue } from "../../../state/StateProvider";
+import {useStateValue} from "../../../state/StateProvider";
 import "./style.css";
 
 function Sidebar() {
-    const [channels, setChannels] = useState([]);
-    const [{user}] = useStateValue();
+	const [channels, setChannels] = useState([]);
+	const [{user}] = useStateValue();
 
-    useEffect(() => {
-        // Run this code once
-        db.collection("rooms").onSnapshot(snapshot => (
-            setChannels(snapshot.docs.map(doc => ({
-                id: doc.id,
-                name: doc.data().name,
-            })))
-        ));
-    }, []);
+	useEffect(() => {
+		// Run this code once
+		db.collection("rooms").onSnapshot((snapshot) => (
+			setChannels(snapshot.docs.map((doc) => ({
+				id: doc.id,
+				name: doc.data().name,
+			})))
+		));
+	}, []);
 
-    return (
-        <div className="sidebar">
-            <div className="sidebar__header">
-                <div className="sidebar__info">
-                    <h2>Krishal Shah</h2>
-                    <h3>
-                        <FiberManualRecordRounded />
-                        {user?.displayName}
-                    </h3>
-                </div>
-                <CreateRounded />
-            </div>
-            <SidebarOption Icon={InsertCommentRounded} title="Threads" />
-            <SidebarOption Icon={InboxRounded} title="Mentions & reactions" />
-            <SidebarOption Icon={DraftsRounded} title="Saved items" />
-            <SidebarOption Icon={BookmarkBorderRounded} title="Channel Browser" />
-            <SidebarOption Icon={PeopleAltRounded} title="People & user groups" />
-            <SidebarOption Icon={AppsRounded} title="Apps" />
-            <SidebarOption Icon={FileCopyRounded} title="File browser" />
-            <SidebarOption Icon={ExpandLessRounded} title="Show less" />
-            <hr />
-            <SidebarOption Icon={ExpandMoreRounded} title="Channels" />
-            <hr />
-            <SidebarOption Icon={AddRounded} title="Add Channel" addChannelOption />
-            {channels.map((channel, index) => (
-                <SidebarOption key={index} title={channel.name} id={channel.id} />
-            ))}
-        </div>
-    )
+	return (
+		<div className="sidebar">
+			<div className="sidebar__header">
+				<div className="sidebar__info">
+					<h2>Krishal Shah</h2>
+					<h3>
+						<FiberManualRecordRounded />
+						{user?.displayName}
+					</h3>
+				</div>
+				<CreateRounded />
+			</div>
+			<SidebarOption Icon={InsertCommentRounded} title="Threads" />
+			<SidebarOption Icon={InboxRounded} title="Mentions & reactions" />
+			<SidebarOption Icon={DraftsRounded} title="Saved items" />
+			<SidebarOption Icon={BookmarkBorderRounded} title="Channel Browser" />
+			<SidebarOption Icon={PeopleAltRounded} title="People & user groups" />
+			<SidebarOption Icon={AppsRounded} title="Apps" />
+			<SidebarOption Icon={FileCopyRounded} title="File browser" />
+			<SidebarOption Icon={ExpandLessRounded} title="Show less" />
+			<hr />
+			<SidebarOption Icon={ExpandMoreRounded} title="Channels" />
+			<hr />
+			<SidebarOption Icon={AddRounded} title="Add Channel" addChannelOption />
+			{channels.map((channel, index) => (
+				<SidebarOption key={index} title={channel.name} id={channel.id} />
+			))}
+		</div>
+	);
 }
 
-export default Sidebar
+export default Sidebar;
